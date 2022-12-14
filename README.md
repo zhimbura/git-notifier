4d55d656ed471dfb03696c0df8395290a74f7a75
Устинов Тихон <tih.ustinov2013@yandex.ru>
2022-12-14 23:46:57 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Telegram бот для уведомлений
+
+Перенос проекта с JS версии [gitlab-bot-webhook](https://github.com/Tihon-Ustinov/gitlab-bot-webhook) на Kotlin
+
+## Суть
+
+Суть бота заключается в том чтобы он мог в телеграммах уведомлять о разных событиях в проекте.
+
+## Функции
+- [ ] Связать ник телеграм с ником git
+
+### GitLab
+- [ ] События GitLab Pipeline
+- [ ] Событие MergeRequest
+
+### GitHub
+- [ ] События GitHub Action
+- [ ] События PullRequest
---
+++ server/src/test/kotlin/com/tikhon/ApplicationTest.kt
@@ -0,0 +1,21 @@
+package com.tikhon
+
+import io.ktor.http.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+import com.tikhon.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
